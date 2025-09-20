f4c9bc0d76bc0f7da87499d9affdbff1a447764f
Okuhle Charlieman <okuhlecharlieman72@gmail.com>
2025-09-20 23:31:05 +0200

    home and layoutpage

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -1,12 +1,11 @@
+
 export default function NotFound() {
   return (
-    <html>
-      <body>
-        <div style={{ textAlign: "center", padding: "4rem" }}>
-          <h1 style={{ fontSize: "2rem", fontWeight: "bold" }}>404 - Page Not Found</h1>
-          <p>Eish sorry, the page you’re looking for does not exist.</p>
-        </div>
-      </body>
-    </html>
+    <div style={{ textAlign: "center", padding: "4rem" }}>
+      <h1 style={{ fontSize: "2rem", fontWeight: "bold" }}>
+        404 - Page Not Found
+      </h1>
+<p>Eish sorry, the page you’re looking for does not exist.</p>
+    </div>
   );
 }
